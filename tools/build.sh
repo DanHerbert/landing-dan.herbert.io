42a5777bf2fcb7e2139e6d8f2b7153dca5d3bfa8
Dan Herbert <web-dan@hrbrt.co>
2023-05-11 11:38:11 -0700

    Add humans.txt file

--- tools/build.sh
+++ tools/build.sh
@@ -7,6 +7,7 @@ project_root=$( cd -- "$script_dir/.." &> /dev/null && pwd)
 src_root="$project_root/src"
 public_root="$project_root/public"
 cp "$src_root"/images/* "$project_root"/public/
+cp "$src_root"/humans.txt "$project_root"/public/
 npx stylus --compress --out "$public_root" "$src_root/main.styl"
 npx pug --pretty --path "$public_root" --basedir "$public_root" \
     --out "$public_root" "$src_root/index.pug"
