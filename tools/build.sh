20ac596c0292f43d88c269ac2cc9c5f5b10ca09a
Dan Herbert <web-dan@hrbrt.co>
2023-08-14 19:06:57 -0700

    Build script adjustments

--- tools/build.sh
+++ tools/build.sh
@@ -6,8 +6,11 @@ script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 project_root=$( cd -- "$script_dir/.." &> /dev/null && pwd)
 src_root="$project_root/src"
 public_root="$project_root/public"
-cp "$src_root"/images/* "$project_root"/public/
-cp "$src_root"/humans.txt "$project_root"/public/
+mkdir -p "$public_root"
+cp "$src_root"/images/* "$public_root/"
+cp "$src_root"/*.txt "$public_root/"
+
 npx stylus --compress --out "$public_root" "$src_root/main.styl"
-npx pug --pretty --path "$public_root" --basedir "$public_root" \
-    --out "$public_root" "$src_root/index.pug"
+npx --package @anduh/pug-cli pug3 --pretty --path "$public_root" --basedir "$public_root" \
+        --out "$public_root" "$src_root/index.pug"
+rm "$public_root/main.css"
