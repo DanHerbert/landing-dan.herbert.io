8dc1c5058d3dbe813e15d7e4f5cae5649311f278
Dan Herbert <web-dan@hrbrt.co>
2023-08-20 13:11:44 -0700

    Change shell tools to behave like npm wrappers
    Some steps within publish.sh still make sense to do within a shell
    script but the rest will be more resilient to changes caused by future
    git commits since the scripts run within the repo they update.

--- tools/build.sh
+++ tools/build.sh
@@ -4,13 +4,6 @@ set -eux
 
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 project_root=$( cd -- "$script_dir/.." &> /dev/null && pwd)
-src_root="$project_root/src"
-public_root="$project_root/public"
-mkdir -p "$public_root"
-cp "$src_root"/images/* "$public_root/"
-cp "$src_root"/*.txt "$public_root/"
 
-npx stylus --compress --out "$public_root" "$src_root/main.styl"
-npx --package @anduh/pug-cli pug3 --pretty --path "$public_root" --basedir "$public_root" \
-        --out "$public_root" "$src_root/index.pug"
-rm "$public_root/main.css"
+cd "$project_root"
+npm run build
--- tools/publish.sh
+++ tools/publish.sh
@@ -7,8 +7,5 @@ set -euv
 
 cd "$project_root"
 git pull --force
-old_files=$(ls -A "$project_root/public/")
-"$project_root/tools/build.sh"
-cd /srv/www/dan.herbert.io/www-root/ && rm "$old_files"
-cd -
-cp -r "$project_root/public/" "/srv/www/dan.herbert.io/www-root/"
+npm install
+npm run publish
