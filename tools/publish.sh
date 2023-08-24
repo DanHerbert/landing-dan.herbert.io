30c806e9295b719761859bca3f592f5c1a8b21f2
Dan Herbert <web-dan@hrbrt.co>
2023-08-24 11:09:48 -0700

    More robust publish launcher script
    Addresses 2 things:
    1. git doesn't like doing things to a repository it doesn't own, but
       this launcher runs as root through a systemd service so the commands
       need to be forced into the correct user for the current machine.
    2. Bash intentionally loads scripts line-by-line, which is bad in this
       case because the script does a git pull, which could modify itself
       while running. The entire script needs to be loaded into memory at
       once to be resistant to changes.

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-project_root=$( cd -- "$script_dir/.." &> /dev/null && pwd)
+# This script can update itself while running (since it updates through git)
+# All commands must happen within these curly brace blocks to ensure everything
+# loads into memory before executing.
+{
+script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+project_root=$(cd -- "$script_dir/.." &>/dev/null && pwd)
 
 set -euv
 
 cd "$project_root"
-if [[ $(git status --porcelain | wc -l) -gt 0 ]]; then
-    git stash push --include-untracked --message "Publishing content $(date -u '+%Y-%m-%dT%H:%M:%SZ')"
+# This script runs as root through systemd, but git doesn't like operating on
+# repositories that it does not own, hence the sudo stuff for git here.
+project_user=$(stat -c '%U' ./)
+if [[ $(sudo -u "$project_user" git status --porcelain | wc -l) -gt 0 ]]; then
+    sudo -u "$project_user" git stash push --include-untracked \
+        --message "Publishing content $(date -u '+%Y-%m-%dT%H:%M:%SZ')"
 fi
-if [[ $(git branch --show-current) != 'main' ]]; then
-    git checkout --force main
+if [[ $(sudo -u "$project_user" git branch --show-current) != 'main' ]]; then
+    sudo -u "$project_user" git checkout --force main
 fi
-git pull --force
+sudo -u "$project_user" git pull --force
 npm install
 npm run publish
+}; exit
